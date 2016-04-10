e29272f107d3c901aad8ea03596426111cb290b5
Sriram <sriramrb@Srirams-MacBook-Pro.local>
2016-04-10 23:58:12 +0530

    added files for boom app

--- boom app/ViewController.swift
+++ boom app/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coollogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var uncoolButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +25,11 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        coollogo.hidden = false
+        coolBg.hidden = false
+        uncoolButton.hidden = true
+        
+    }
 }
 
